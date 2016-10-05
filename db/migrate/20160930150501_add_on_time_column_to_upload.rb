e9fb1d0b720f46c90241979fd45e2f523c87cd97
ellenlee <leechinghui.tw@gmail.com>
2016-10-05 12:38:48 +0800

    整理migration

--- db/migrate/20160930150501_add_on_time_column_to_upload.rb
+++ db/migrate/20160930150501_add_on_time_column_to_upload.rb
@@ -1,7 +1,5 @@
 class AddOnTimeColumnToUpload < ActiveRecord::Migration[5.0]
   def change
   	add_column :uploads, :on_time, :boolean
-  	remove_column :uploads, :status_id
-  	# drop_table :upload_statuses
   end
 end
